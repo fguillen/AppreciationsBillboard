5e3a88f421a6a395d4aeed8fcf9bd7f7f0ad79ea
rgould <rwgould@gmail.com>
2018-11-09 11:40:04 +0100

    Add migrations

---
+++ db/migrate/20180406140125_create_admin_user.rb
@@ -0,0 +1,16 @@
+class CreateAdminUser < ActiveRecord::Migration[5.1]
+  def change
+    create_table :admin_users do |t|
+      t.string :uuid, :null => false
+      t.string :name, :null => false
+      t.string :email, :null => false
+
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :perishable_token
+      t.string :persistence_token
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201811021315) do
+ActiveRecord::Schema.define(version: 2018_04_06_140445) do
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "uuid", null: false
     t.string "name", null: false
