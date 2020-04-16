bae203495f644dfa03347224993b652834434db1
rgould <rwgould@gmail.com>
2020-04-16 13:02:27 +0200

    Support renaming rails_skeleton files

--- lib/renaming_project.rb
+++ lib/renaming_project.rb
@@ -1,11 +1,25 @@
 class RenamingProject
   def self.perform(new_name)
     Dir.glob("#{Rails.root}/**/*.*").each do |filepath|
+      next unless File.file? filepath
+
+      new_filepath = if filepath =~ /rails_skeleton/
+                       filepath.gsub("rails_skeleton", new_name.underscore)
+                     else
+                       filepath
+                     end
+
       file_content = File.read(filepath)
       file_content = file_content.gsub("RailsSkeleton", new_name)
       file_content = file_content.gsub("railsskeleton", new_name.downcase)
 
-      File.open(filepath, "w") { |f| f.write file_content }
+      FileUtils.mkdir(File.dirname(new_filepath)) unless Dir.exists?(File.dirname(new_filepath))
+      File.write(new_filepath, file_content)
+
+      if new_filepath != filepath
+        `git rm "#{filepath}"`
+      end
+
     end
   end
 end
