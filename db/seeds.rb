9a4bc9a4ec4b13e7217a1abbce70811431d33deb
Fernando Guillen <fguillen.mail@gmail.com>
2020-04-18 18:01:39 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,35 @@
 password = "p4sswORd"
 email = "admin@example.com"
 
-AdminUser.create!(
-  :name => "Super Admin",
-  :email => email,
-  :password => password,
-  :password_confirmation => password
-)
+if !AdminUser.where(email: email).exists?
+  AdminUser.create!(
+    :name => "Super Admin",
+    :email => email,
+    :password => password,
+    :password_confirmation => password
+  )
+  puts "AdminUser created #{email}/#{password}"
+end
 
-puts "AdminUser created #{email}/#{password}"
+puts "Creating AppreciableUser"
+10.times do
+  AppreciableUser.create!(
+    name: Faker::FunnyName.unique.name,
+    email: Faker::Internet.unique.email
+  )
+  print "."
+end
+puts
+
+puts "Creating Appreciations"
+100.times do
+  appreciable_users_to = (rand(2) + 1).times.map { AppreciableUser.all.sample }
+  num_sentences = rand(3) + 1
+
+  Appreciation.create!(
+    by: AppreciableUser.all.sample,
+    to: appreciable_users_to,
+    message: Faker::Markdown.sandwich(sentences: num_sentences)
+  )
+  print "."
+end
